663cc7cc245c0358bc58817d680f5cf72b52c42c
naveenkumar-4 <naveenkumarrockz362@gmail.com>
2021-11-10 11:40:16 +0530

    print statement is updated

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ def check_palindrome(s):
 	return False
 s = input()
 if check_palindrome(s) == True:
-	print(" Given is the  Palindrome")
+	print(" Given no  is   Palindrome")
 else:
 	print("Not Palindrome")
